11e3aede306c73c9a36c517c045a0e84d059ba35
EletNavigator <eletnavigator.kapcsolat@gmail.com>
2026-07-01 00:40:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-const CACHE_NAME = "eletnavigator-v3";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./css/style.css",
-  "./manifest.json",
-  "./icon.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", (e) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (e) => {
+  console.log("PWA aktív");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request));
 });
